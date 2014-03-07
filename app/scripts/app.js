ceefab38396d545cabab4f0c008c8369ee2ab7d0
Michael Stanley <mlstanley@gmail.com>
2014-03-07 13:45:49 -0800

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('MyFBPlaygroundApp', [
+  'ngRoute',
+  'firebase',
+  'angularfire.firebase',
+  'angularfire.login',
+  'simpleLoginTools'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/login', {
+        authRequired: false, // if true, must log in before viewing this page
+        templateUrl: 'views/login.html',
+        controller: 'LoginController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('MyFBPlaygroundApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
